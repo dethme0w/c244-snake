2938fe4b193199b09f70414464d444e5d3cd52a5
Jackson Williams <jackson.r.williams+github@gmail.com>
2013-01-25 00:26:46 -0800

    Added snake change direction tests.  Some tests fail.  Someone else can fix Snake.java so they pass.

---
+++ Snake/src/ca/camosun/snake/SnakeTest.java
@@ -0,0 +1,55 @@
+package ca.camosun.snake;
+
+import static org.junit.Assert.*;
+import static ca.camosun.snake.Snake.Direction.*;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SnakeTest {
+	List<Fruit> noFruit;
+	
+	@Before
+	public void setUp() throws Exception {
+		noFruit = Collections.emptyList();
+	}
+
+	@Test
+	public void testChangeDirectionOnlyAfterMove() {
+		Snake snake = new Snake(NORTH);
+		
+		snake.changeDirection(EAST);
+		assertEquals(NORTH, snake.getCurrentDirection());
+		
+		snake.moveSnake(noFruit);
+		assertEquals(EAST, snake.getCurrentDirection());
+	}
+	
+	@Test
+	public void testChangeDirectionNotOpposite() {
+		Snake snake = new Snake(NORTH);
+		
+		snake.changeDirection(SOUTH);
+		
+		snake.moveSnake(noFruit);
+		assertEquals(NORTH, snake.getCurrentDirection());
+	}
+	
+	@Test
+	public void testChangeDirectionTwiddlingDirectionsDoesntCircumventNotOpposite() {
+		/* consider snake moving north.  
+		 * user presses right then down before snake moves.  
+		 * on next move, snake shouldn't move down through itself. */
+		
+		Snake snake = new Snake(NORTH);
+		
+		snake.changeDirection(EAST);
+		snake.changeDirection(SOUTH);
+		
+		snake.moveSnake(noFruit);
+		assertEquals(EAST, snake.getCurrentDirection());
+	}
+}
