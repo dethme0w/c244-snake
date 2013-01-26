d929360bc2c8014363fdd6909bdc2e609887e42b
Dan <dmel.c132@gmail.com>
2013-01-25 17:40:35 -0800

    Added "start game" button. Initialized Snake Board on the screen by treating it as a view. Board Size assumed to be full screen by default.

--- Snake/src/ca/camosun/snake/MainActivity.java
+++ Snake/src/ca/camosun/snake/MainActivity.java
@@ -3,14 +3,46 @@ package ca.camosun.snake;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private View snakeBoard;
+	private Button startGame;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		snakeBoard = new SnakeBoard(this);
+		setContentView(snakeBoard);
+		snakeBoard.requestFocus();
+		
+		//startGame = (Button)findViewById(R.id.startGame);	
+		//startGame.setOnClickListener(runGame); 
+		
+
+	
 	}
+	
+	/* 
+	 private OnClickListener runGame = new OnClickListener() {
+			public void onClick(View v) {
+				
+			//TODO: Code for the Start Game button needs to be linked to the SnakeBoard View
+			// instead of starting when you open the app
+	                     	
+	        }
+	    };
+	    
+	    */
+	
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- Snake/src/ca/camosun/snake/SnakeBoard.java
+++ Snake/src/ca/camosun/snake/SnakeBoard.java
@@ -3,24 +3,44 @@ package ca.camosun.snake;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SnakeBoard {
-	private int sizeX;
-	private int sizeY;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class SnakeBoard extends View {
+
 	private Snake snake;
 	private List<Fruit> fruits;
 	private int score;
-
-	public SnakeBoard(int inSizeX, int inSizeY) {
-		sizeX = inSizeX;
-		sizeY = inSizeY;
+	
+	public SnakeBoard(Context context) {
+		super(context);
+		setFocusable(true);
+        
 		snake = new Snake();
 		fruits = new ArrayList<Fruit>();
 		score = 0;
+		
+		this.setBackgroundColor(Color.GREEN);
+
+		
+		
 	}
+	
+	// TODO: Draw all graphics in here such as the board tiles
+	@Override
+    public void onDraw(Canvas canvas) {
+		
+		
+
+    }
+
 
 	
-	// TODO: Method to initialize the board
-	// Method to detect a collision between the snake and the edge
+	// TODO: Method to detect a collision between the snake and the edge
 	
 
 }
