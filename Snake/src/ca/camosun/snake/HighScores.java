dd683295b7737b98039936e6e4e019cebd7e458b
AldenJones <Aldenjones65@gmail.com>
2013-01-30 13:47:17 -0800

    Added the HighScores and Score classes Highscores keeps an ArrayList of scores.

---
+++ Snake/src/ca/camosun/snake/HighScores.java
@@ -0,0 +1,41 @@
+package ca.camosun.snake;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class HighScores implements Iterable<Score> {
+	private List<Score> highScores;
+	int scoresToKeep = 10;
+	
+	public HighScores(){
+		highScores = new ArrayList<Score>();	
+	}
+
+	
+	@Override
+	public Iterator<Score> iterator() {
+		Collections.sort(highScores);
+		Collections.reverse(highScores);
+		
+		return highScores.iterator();
+	}
+	
+	public void addScore(String inName, int inScore){
+		
+		Score newScore = new Score(inName, inScore);
+		
+		if(highScores.size() == scoresToKeep){
+			int comparison = newScore.compareTo(Collections.min(highScores));
+			if(comparison > 0){
+				highScores.remove(Collections.min(highScores));
+			}
+			
+		}
+		
+		highScores.add(newScore);
+		Collections.sort(highScores);
+		Collections.reverse(highScores);
+	}
+}
---
+++ Snake/src/ca/camosun/snake/Score.java
@@ -0,0 +1,29 @@
+package ca.camosun.snake;
+
+public class Score implements Comparable<Score> {
+	
+	private int score;
+	private String name;
+	
+	public Score(String inName, int inScore){
+		name = inName;
+		score = inScore;
+		
+	}
+			
+	@Override
+	public int compareTo(Score newScore) {
+		int difference = score - newScore.score;	
+		return difference;
+		
+	}
+	
+	public int getScore(){
+		return score;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+}
