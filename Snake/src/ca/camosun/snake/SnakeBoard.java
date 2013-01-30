41da2a700268433e3bf1051dac6101f218132f51
Gerald <gerald.albion@gmail.com>
2013-01-30 10:19:10 -0800

    Add unit test

--- Snake/src/ca/camosun/snake/SnakeBoard.java
+++ Snake/src/ca/camosun/snake/SnakeBoard.java
@@ -10,18 +10,27 @@ public class SnakeBoard {
 	private Snake snake;
 	private List<Fruit> fruits;
 	private int score;
+	private int maxX;
+	private int maxY;
 	
-	public SnakeBoard() {
-		
+	public SnakeBoard(int inMaxX, int inMaxY) {
+		maxX = inMaxX;
+		maxY = inMaxY;
 		snake = new Snake(NORTH, 0, 0);
 		fruits = new ArrayList<Fruit>();
-		score = 0;
-		
-		
+		score = 0;	
 	}
 			
 	// TODO: Method to detect a collision between the snake and the edge
 	
-	
+	public boolean wentOffBoard() {
+		
+		return false;
+	}
+
+	public Snake getSnake() {
+		return snake;
+	}
+
 
 }
---
+++ Snake/src/ca/camosun/snake/test/SnakeBoardTest.java
@@ -0,0 +1,48 @@
+package ca.camosun.snake.test;
+
+import static ca.camosun.snake.Snake.Direction.NORTH;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.camosun.snake.Snake;
+import ca.camosun.snake.SnakeBoard;
+import ca.camosun.snake.SnakeSegment;
+
+public class SnakeBoardTest {
+	
+	Snake snake;
+	SnakeSegment snakeHead; 
+	SnakeBoard board;
+	
+	@Before
+	public void setUp() throws Exception {
+		board = new SnakeBoard(49,49);
+		snake = board.getSnake();
+	}
+
+	@Test
+	public void snakeWentOffBoard() {
+				
+		// Try to go south (will run off board)
+		snake.changeDirection(Snake.Direction.SOUTH);
+		assertTrue(board.wentOffBoard());
+		
+		// Try to go north (will not run off board)
+		snake.changeDirection(Snake.Direction.NORTH);
+		assertFalse(board.wentOffBoard());
+		
+		// Try to go west (will run off board)
+		snake.changeDirection(Snake.Direction.WEST);
+		assertTrue(board.wentOffBoard());
+		
+		// Try to go east (will not run off board)
+		snake.changeDirection(Snake.Direction.EAST);
+		assertFalse(board.wentOffBoard());
+		
+		
+		
+	}
+
+}
