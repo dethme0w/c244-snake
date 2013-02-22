fe2f92b45d439a9c745183a412f8c71aa0b872ea
gerald.albion <gerald.albion@gmail.com>
2013-02-21 22:58:06 -0800

    Issue 55: Moved sensor input code directly into test activity. Eliminate tilt sensor unit.  More on why in the Issue.

--- Snake/src/ca/camosun/snake/TiltTestActivity.java
+++ Snake/src/ca/camosun/snake/TiltTestActivity.java
@@ -3,37 +3,39 @@ package ca.camosun.snake;
 import java.util.Timer;
 import java.util.TimerTask;
 import ca.camosun.snake.R;
-import ca.camosun.snake.TiltInput;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class TiltTestActivity extends Activity {
+public class TiltTestActivity extends Activity implements SensorEventListener {
 
-	private static final int REFRESH_TIME = 250; // ms
-	private TiltInput sensor;
+	private static final int REFRESH_TIME = 250; // ms	
 	private Timer mTimer;
     private TextView mTextView;
-    private int count;
-    
+    private SensorManager mSensorManager;
+	private Sensor mSensor;
+	private float x;
+	private float y;
     
 	private void updateDisplay() {
-		String tiltDirection = sensor.getTilt();
-		count++;
-		mTextView.setText(tiltDirection+" "+count);
+		String tiltDirection = getTilt();			
+		mTextView.setText(tiltDirection);
 	}
-
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tilt_test);
 		mTextView = (TextView) findViewById(R.id.tvRaw);
-		sensor = new TiltInput();
-		
+						
 		mTimer = new Timer();
 		mTimer.scheduleAtFixedRate(new TimerTask() {
 			@Override
@@ -47,7 +49,17 @@ public class TiltTestActivity extends Activity {
 			}
 		}, 0, REFRESH_TIME);
         mTextView.setText("X");
+        
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
 	}
+	
+	 @Override
+	    protected void onResume() {
+	      super.onResume();
+	      mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,6 +70,36 @@ public class TiltTestActivity extends Activity {
 
 	public void doneClicked(View view) {
 		this.finish();
+	}
+	
+	public String getTilt() {
+		
+		if (Math.abs(x) > Math.abs(y)) {
+			// tilt is horizontal
+			if (x > 0) {
+				return ("L");
+			}
+			return ("R");
+		}
+
+		// tilt is vertical
+		if (y > 0) {
+			return ("U");
+		}
+		return ("D");
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		x = event.values[0];
+		y = event.values[1];		
 	}
 
 }
