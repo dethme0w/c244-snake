c587d84fab29b3dacf1e5f4317521b94059cadc6
Gerald <gerald.albion@gmail.com>
2013-03-06 09:53:57 -0800

    rm TiltTestActivity.  It has served its purpose but is no longer needed.

--- Snake/src/ca/camosun/snake/gui/MainActivity.java
+++ Snake/src/ca/camosun/snake/gui/MainActivity.java
@@ -2,7 +2,6 @@ package ca.camosun.snake.gui;
 
 import ca.camosun.snake.ImageAdapter;
 import ca.camosun.snake.R;
-import ca.camosun.snake.TiltTestActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -31,19 +30,15 @@ public class MainActivity extends Activity {
 	}
 	
 	/** Called when the user clicks the Send button */
-	public void startGame(View view) {
-	    System.out.println("Start button pressed");
-	    Intent intent = new Intent(this, GameActivity.class);
-	    
+	public void startGame(View view) {	    
+	    Intent intent = new Intent(this, GameActivity.class);	    
 	    startActivity(intent);
 	}
 	
-	public void testTiltClicked(View view) {
-	    System.out.println("Test Tilt button pressed");
-	    Intent intent = new Intent(this, TiltTestActivity.class);	    
-	    startActivity(intent);
+	public void quit(View view) {
+		MainActivity.this.finish();
 	}
-	
+			
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
